2a467e283891e4107a82d3971f3439f574cb784b
Jacob Costner <jakee248@gmail.com>
2021-03-22 21:43:33 -0400

    added STRING functionality, built parts of others

--- Driver.java
+++ Driver.java
@@ -11,24 +11,12 @@ public class Driver {
         ANTLRInputStream input = new ANTLRInputStream(System.in);
 // create a lexer that feeds off of input CharStream
         LittleLexer lexer = new LittleLexer(input);
-	lexer.removeErrorListeners();
-	lexer.addErrorListener(ThrowingErrorListener.INSTANCE);
 // create a buffer of tokens pulled from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
 	LittleParser parser = new LittleParser(tokens);
-	parser.removeErrorListeners();
-	parser.addErrorListener(ThrowingErrorListener.INSTANCE);
-	
-	try{
 	ParseTree tree = parser.program();
-	}
-	catch(ParseCancellationException e){
-		System.out.print("Not Accepted\n");
-		return;
-	}
-
-
+	
 // Create a generic parse tree walker that can trigger callbacks
 	 ParseTreeWalker walker = new ParseTreeWalker();
 	 SymbolTableExtractor ste = new SymbolTableExtractor();
--- SymbolTableExtractor.java
+++ SymbolTableExtractor.java
@@ -1,7 +1,22 @@
+import org.antlr.v4.runtime.*;
+import java.util.*;
+import java.io.*;
+
 public class SymbolTableExtractor extends LittleBaseListener{
 
+@Override
+public void enterString_decl(LittleParser.String_declContext ctx){
 
+}
 
+@Override
+public void exitString_decl(LittleParser.String_declContext ctx){
+String text1 = ctx.getText();
+String text2 = text1.substring(6, text1.length()-1);
+String[] tokens = text2.split(":=");
+String name = tokens[0];
+String value = tokens[1];
+System.out.printf("name %s type STRING value %s\n", name, value);
 
 }
 
@@ -9,5 +24,7 @@ public class SymbolTable {
         String name;
         String type;
         String value;
-        Hashtable<int, SymbolTable> ht = new Hashtable<>();
+        Hashtable<Integer, SymbolTable> ht = new Hashtable<>();
+}
+
 }
